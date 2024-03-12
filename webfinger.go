96b56ad384e99eb42c0c18ba1321d3c9a0d74c1c
Oliver Lowe <o@olowe.co>
2024-03-12 20:40:27 +1100

    apub: remove unused UserWebFingerFile
    Moved to package sys, generated dynamically (and tested!).

--- webfinger.go
+++ webfinger.go
@@ -47,25 +47,3 @@ func fingerAll(alist []*mail.Address) ([]Actor, error) {
 	}
 	return actors, nil
 }
-
-func UserWebFingerFile(username string) (string, error) {
-	u, err := user.Lookup(username)
-	if err != nil {
-		return "", err
-	}
-	if u.HomeDir == "" {
-		return "", fmt.Errorf("no home directory")
-	}
-
-	paths := []string{
-		path.Join(u.HomeDir, "lib/webfinger"),                 // Plan 9
-		path.Join(u.HomeDir, ".config/webfinger"),             // Unix-like
-		path.Join(u.HomeDir, "Application Support/webfinger"), // macOS
-	}
-	for i := range paths {
-		if _, err := os.Stat(paths[i]); err == nil {
-			return paths[i], nil
-		}
-	}
-	return "", fmt.Errorf("no webfinger file")
-}
